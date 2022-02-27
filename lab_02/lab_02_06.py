6e11b4bfa77575617ea0188ac5c083404f25b97d
Alexey <zav20u200@student.bmstu.ru>
2022-02-27 16:05:02 +0300

    3

--- lab_02/lab_02_06.py
+++ lab_02/lab_02_06.py
@@ -12,25 +12,26 @@ OY = 400
 Total_kx = 1.0
 Total_ky = 1.0
 
+ZOOM_IN_EVENT_K = 1.1
+ZOOM_OUT_EVENT_K = 0.9
+MOVE_EVENT_D = 10
+ROT_EVENT_ANGLE = 0.1
+
+
 PICTURE = []
 PICTURE_LOG = []
 PICTURE_LOG_MAX_LEN = 50
-WIN_WIDTH = 1400
-WIN_HEIGHT = 900
-WIN_COLOR = "#66b3ff"
+
+LABEL_COLOR = "#66b3ff"
 
 CV_WIDE = 1000
 CV_HEIGHT = 800
-СV_COLOR = "#cce6ff"
-
+CV_COLOUR = 'old lace'
 
 TEXT_COLOR = "lightblue"
 
-X_CENTER = 0
-Y_CENTER = 0
-
 win = Tk()
-canv = Canvas(win, width=800, height=800, bg='gray')
+canv = Canvas(win, CV_WIDE, CV_HEIGHT, bg=CV_COLOUR)
 canv.pack(expand=True, fill=BOTH)
 
 OY_LINE = canv.create_line(OY - 1000000, OX, OY + 1000000, OX)
@@ -40,6 +41,7 @@ ZERO = canv.create_oval(OY, OX, OY, OX, state=HIDDEN)
 def init_picture():
     canv.delete(ALL)
     global PICTURE; PICTURE.clear()
+    global PICTURE_LOG; PICTURE_LOG.clear()
     global OY_LINE; OY_LINE = canv.create_line(OY - 1000000, OX, OY + 1000000, OX)
     global OX_LINE; OX_LINE = canv.create_line(OY, OX - 1000000, OY, OX + 1000000)
     global ZERO; ZERO = canv.create_oval(OY, OX, OY, OX, state=HIDDEN)
@@ -122,13 +124,13 @@ def rotate(angle, xc=None, yc=None, text_rot=False):
 def zoom_event(event):
     global Total_ky, Total_kx
     if (event.delta > 0):
-        zoom(1.1, 1.1, event.x, event.y)
-        Total_kx *= 1.1
-        Total_ky *= 1.1
+        zoom(ZOOM_IN_EVENT_K, ZOOM_IN_EVENT_K, event.x, event.y)
+        Total_kx *= ZOOM_IN_EVENT_K
+        Total_ky *= ZOOM_IN_EVENT_K
     elif (event.delta < 0):
-        zoom(0.9, 0.9, event.x, event.y)
-        Total_kx *= 0.9
-        Total_ky *= 0.9
+        zoom(ZOOM_OUT_EVENT_K, ZOOM_OUT_EVENT_K, event.x, event.y)
+        Total_kx *= ZOOM_OUT_EVENT_K
+        Total_ky *= ZOOM_OUT_EVENT_K
 
 def zoom(kx, ky, xc, yc):
     global PICTURE_LOG
@@ -208,7 +210,9 @@ def move_io():
 def focus(event):
     canv.focus_set()
 
-def step_backing():
+def step_backing(text_rot=False):
+    if (not text_rot and win.focus_get() != canv):
+        return
     if not len(PICTURE_LOG):
         return
     pic_copy = [ZERO, OX_LINE, OY_LINE] + PICTURE.copy()
@@ -216,24 +220,26 @@ def step_backing():
         canv.coords(pic_copy[i], tuple(PICTURE_LOG[-1][i]))
     PICTURE_LOG.pop()
 
+def aboutprog():
+    mb.showinfo(title='О программе', message='Нарисовать исходный рисунок, затем его переместить,\
+промасштабировать, повернуть')
+
 init_picture()
 focus(None)
 
-win['bg'] = WIN_COLOR
-win.geometry("%dx%d" %(WIN_WIDTH, WIN_HEIGHT))
 win.attributes("-fullscreen", True)
 
 # Center
-center_label = Label(win, text = "Центр(для масштабирования и поворота)", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+center_label = Label(win, text = "Центр(для масштабирования и поворота)", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 center_label.place (x = CV_WIDE + 15, y = 20)
 
-center_x_label = Label(win, text = "X:", font="-family {Consolas} -size 14", bg = WIN_COLOR)
+center_x_label = Label(win, text = "X:", font="-family {Consolas} -size 14", bg = LABEL_COLOR)
 center_x_label.place(x = CV_WIDE + 70, y = 50)
 center_x = Entry(win, font="-family {Consolas} -size 14", width = 9)
 center_x.insert(END, "0")
 center_x.place (x = CV_WIDE + 100, y = 50)
 
-center_y_label = Label(win, text = "Y:", font="-family {Consolas} -size 14", bg = WIN_COLOR)
+center_y_label = Label(win, text = "Y:", font="-family {Consolas} -size 14", bg = LABEL_COLOR)
 center_y_label.place(x = CV_WIDE + 270, y = 50)
 center_y = Entry(win, font="-family {Consolas} -size 14", width = 9)
 center_y.insert(END, "0")
@@ -243,7 +249,7 @@ center_y.place (x = CV_WIDE + 300, y = 50)
 spin_label = Label(win, text = "Поворот", width = 36, font="-family {Consolas} -size 18", bg = TEXT_COLOR)
 spin_label.place(x = CV_WIDE + 1, y = 110)
 
-spin_angle_label = Label(win, text = "Угол°: ", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+spin_angle_label = Label(win, text = "Угол°: ", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 spin_angle_label.place(x = CV_WIDE + 160, y = 155)
 spin_angle = Entry(win, font="-family {Consolas} -size 16", width = 9)
 spin_angle.insert(END, "0")
@@ -256,13 +262,13 @@ spin_btn.place(x = CV_WIDE + 160, y = 200)
 scale_label = Label(win, text = "Масштабирование", width = 36, font="-family {Consolas} -size 18", bg = TEXT_COLOR)
 scale_label.place(x = CV_WIDE + 1, y = 300)
 
-scale_x_label = Label(win, text = "kx: ", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+scale_x_label = Label(win, text = "kx: ", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 scale_x_label.place(x = CV_WIDE + 100, y = 360)
 scale_x = Entry(win, font="-family {Consolas} -size 14", width = 9)
 scale_x.insert(END, "1")
 scale_x.place (x = CV_WIDE + 140, y = 360)
 
-scale_y_label = Label(win, text = "ky: ", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+scale_y_label = Label(win, text = "ky: ", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 scale_y_label.place(x = CV_WIDE + 270, y = 360)
 scale_y = Entry(win, font="-family {Consolas} -size 14", width = 9)
 scale_y.insert(END, "1")
@@ -275,13 +281,13 @@ scale_btn.place(x = CV_WIDE + 160, y = 420)
 move_label = Label(win, text = "Перемещение", width = 36, font="-family {Consolas} -size 18", bg = TEXT_COLOR)
 move_label.place(x = CV_WIDE + 1, y = 520)
 
-move_x_label = Label(win, text = "dx: ", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+move_x_label = Label(win, text = "dx: ", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 move_x_label.place(x = CV_WIDE + 100, y = 580)
 move_x = Entry(win, font="-family {Consolas} -size 14", width = 9)
 move_x.insert(END, "0")
 move_x.place (x = CV_WIDE + 140, y = 580)
 
-move_y_label = Label(win, text = "dy: ", font="-family {Consolas} -size 16", bg = WIN_COLOR)
+move_y_label = Label(win, text = "dy: ", font="-family {Consolas} -size 16", bg = LABEL_COLOR)
 move_y_label.place(x = CV_WIDE + 270, y = 580)
 move_y = Entry(win, font="-family {Consolas} -size 14", width = 9)
 move_y.insert(END, "0")
@@ -293,24 +299,30 @@ move_btn.place(x = CV_WIDE + 160, y = 640)
 quit_btn = Button(win, text="Выход",font="-family {Consolas} -size 14", command = lambda: win.quit(), width = 9, height = 2, bg = TEXT_COLOR )
 quit_btn.place(x = CV_WIDE + 1, y = 760)
 
-
-step_back = Button(win, text = "Шаг назад", font="-family {Consolas} -size 14", command = lambda: step_backing(), width = 15, height = 2, bg = TEXT_COLOR)
+step_back = Button(win, text = "Шаг назад", font="-family {Consolas} -size 14", command = lambda: step_backing(True), width = 15, height = 2, bg = TEXT_COLOR)
 step_back.place(x = CV_WIDE + 120, y = 760)
 
 clear = Button(win, text = "Сбросить", font="-family {Consolas} -size 14", command = lambda: init_picture(), width = 15, height = 2, bg = TEXT_COLOR)
 clear.place(x = CV_WIDE + 300, y = 760)
 
+credits = Button(win, text = "Об авторе", font="-family {Consolas} -size 8", command = lambda: mb.showinfo(title='Об авторе', message='Золотухин Алексей ИУ7-44Б'), width = 15, height = 2, bg = TEXT_COLOR)
+credits.place(x = 10, y = 10)
 
-win.bind('<Up>', lambda event: move(0, -10))
-win.bind('<Right>', lambda event: move(10, 0))
-win.bind('<Left>', lambda event: move(-10, 0))
-win.bind('<Down>', lambda event: move(0, 10))
-win.bind('<w>', lambda event: move(0, -10, True))
-win.bind('<d>', lambda event: move(10, 0, True))
-win.bind('<a>', lambda event: move(-10, 0, True))
-win.bind('<s>', lambda event: move(0, 10, True))
-win.bind('<q>', lambda event: rotate(0.1))
-win.bind('<e>', lambda event: rotate(-0.1))
+about_prog = Button(win, text = "О программе", font="-family {Consolas} -size 8", command = lambda: aboutprog, width = 15, height = 2, bg = TEXT_COLOR)
+about_prog.place(x = 10, y = 55)
+
+
+
+win.bind('<Up>', lambda event: move(0, -MOVE_EVENT_D))
+win.bind('<Right>', lambda event: move(MOVE_EVENT_D, 0))
+win.bind('<Left>', lambda event: move(-MOVE_EVENT_D, 0))
+win.bind('<Down>', lambda event: move(0, MOVE_EVENT_D))
+win.bind('<w>', lambda event: move(0, -MOVE_EVENT_D, True))
+win.bind('<d>', lambda event: move(MOVE_EVENT_D, 0, True))
+win.bind('<a>', lambda event: move(-MOVE_EVENT_D, 0, True))
+win.bind('<s>', lambda event: move(0, MOVE_EVENT_D, True))
+win.bind('<q>', lambda event: rotate(ROT_EVENT_ANGLE))
+win.bind('<e>', lambda event: rotate(-ROT_EVENT_ANGLE))
 win.bind('<BackSpace>', lambda event: step_backing())
 canv.bind('<Button-1>', focus)
 canv.bind('<MouseWheel>', zoom_event)
